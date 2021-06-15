f05f980a6e549ffefb3e6ec641fc40ccb54055ee
Afshana123 <nKafuk2JTFW!ATJ>
2021-06-15 11:38:38 +0100

    Updated Files

--- working_code.py
+++ working_code.py
@@ -1,5 +1,6 @@
 import string
 import random
+import configparser
 
 class PasswordChecker:
 
@@ -30,10 +31,12 @@ class PasswordChecker:
 
         while True:
 
+            # reading through the password policy and looping through to extract necessary values to check and generates the password
             policy_checklist = self.read_password_policy()
             max_length = policy_checklist[5]
             allowed_characters = list(policy_checklist[6])
 
+            # Generating the password using random
             password = ""
             for character in range(max_length):
 
@@ -48,52 +51,73 @@ class PasswordChecker:
                 else:
                     password += random.choice(string.digits)
 
+            # Checking against the user's details, password's policy and the most common password list.
             user_details_check = self.check_user_details(password, user_firstname, user_lastname, user_birthyear)
-            if user_details_check == False:
+            policy_compliance_check = self.check_policy(password)
+            checking_list = self.check_list(password)
+            if user_details_check == False or policy_compliance_check == False or checking_list == False:
                 # print("Regenerating a new password: ") # this is a test to check against user details
                 continue
 
             print(password)
 
-            ask_again = input("Would you like to generate a new password? (y/n) ")
-            if ask_again == "y":
-                continue
-            else:
+            # Shows the generated password and ask the user whether to generate another password using same user's information -
+            # not save the password but maybe send a report for why it is strong
+
+            while True:
+                ask_again = input("Would you like to generate a new password? (y/n) ")
+                if ask_again == "y":
+                    break
+                elif ask_again == "n":
+                    break
+                else:
+                    print("Please input a valid option.")
+
+            if ask_again == "n":
                 break
+
+            # strongpassword.txt - give append or overwrite - ask user if they want to
 
     def check_password(self, password, first_name, second_name, birth_year):
         print("opened check_password")
-        # check password against password policy, their name, DOB, and common passwords.
+        # check password against password policy, their name, DOB, and common passwords. Written by KW
         report = []
+
         policy_compliant = self.check_policy(password)
         # print(f"checked policy compliance {policy_compliant}")
+
         if policy_compliant:
             report.append("Complies with password policy")
             # print("Complies with password policy")
         else:
             report.append("Does not comply with password policy")
             # print("Does not comply with password policy")
+
         not_common = self.check_list(password)
         # print("common passwords checked")
+
         if not_common:
             report.append("Not in list of common passwords")
             # print("Not in list of common passwords")
         else:
             report.append("Found in list of common passwords")
             # print("Found in list of common passwords")
+
         user_detail_free = self.check_user_details(password, first_name, second_name, birth_year)
         if user_detail_free:
             report.append("No user details in password")
         else:
             report.append("User Details Found in Password")
+
         if policy_compliant and not_common and user_detail_free:
             report.append("This means that the password is STRONG.")
         else:
             report.append("This means that the password is weak")
-        self.generate_report(report)
+
+        self.generate_report(report, password)
 
     def check_list(self, password):
-        # checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found
+        # checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found.Written by KW
         with open('common_passwords.txt', 'r') as password_file:
             # iterates through whole file
             for line in password_file:
@@ -108,7 +132,7 @@ class PasswordChecker:
         return True
 
     def check_policy(self, password):
-        # reads password policy, checks if password complies with requirements. Returns True if yes, False if not.
+        # reads password policy, checks if password complies with requirements. Returns True if yes, False if not. Written by KW
         policy_list = self.read_password_policy()
         # print(policy_list)
         # now hav ea list defining password policy
@@ -119,13 +143,16 @@ class PasswordChecker:
         min_length = policy_list[4]
         max_length = policy_list[5]
         allowed_specials = policy_list[6]
+
         count_specials = 0
         count_lower = 0
         count_upper = 0
         count_numbers = 0
+
         if len(password) < min_length or len(password) > max_length:
             # not compliant if too short or too long
             return False
+
         for letter in password:
             # check each letter to see if special, lower, upper, or number. Count each of these
             if letter.isdigit():
@@ -140,6 +167,7 @@ class PasswordChecker:
                 # return false if part of password is not in any allowed category
                 print("illegal character")
                 return False
+
         # now have a count of all the lower, upper, special characters and numbers
         if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
             return True
@@ -147,7 +175,7 @@ class PasswordChecker:
             return False
 
     def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
-        # checks if the password contains the user name or year of birth. Outputs True if no user details in the password
+        # checks if the password contains the user name or year of birth. Outputs True if no user details in the password. Written by KW
         if user_firstname in password:
             return False
         elif user_lastname in password:
@@ -157,34 +185,54 @@ class PasswordChecker:
         else:
             return True
 
-    def generate_report(self, report):
-        # formats output in human-readable way. Checks if user wants this put into a file.
+    def generate_report(self, report, password):
+        # formats output in human-readable way. Checks if user wants this put into a file. Written by KW
+        print(f"Password was: {password}")
         for line in report:
             print(line)
         # ask user if they want to write to file
         wants_file = input("Would you like this written to a file (y/n) ? ")
         if wants_file.lower() == "y":
-            self.write_to_file(report)
+            self.write_to_file(report, password)
         else:
             return "Report not written to file"
 
-    def write_to_file(self, report):
-        # writes report to file.
-        file = open('password_report.txt', 'w')
-        for line in report:
-            file.write(f"{line}\n")
-        return "Report written to password_report.txt"
+    def write_to_file(self, report, password):
+        while True:
+            # writes report to file. can append or overwrite. Written by KW
+            write_type = input("Please select 1 to append, or 2 to overwrite: ")
+
+            if write_type != "1" and write_type != "2":
+                print("Please input a valid option.")
+                continue
+
+            elif write_type == "1":
+                file = open('password_report.txt', 'a')
+
+            elif write_type == "2":
+                file = open('password_report.txt', 'w')
+
+            file.write(f"Password was: {password} \n")
+            for line in report:
+                file.write(f"{line}\n")
+
+            file.write(f"\n")
+            return "Report written to password_report.txt"
+            break
 
     def read_password_policy(self):
-        # reads in password policy, returns variables as a list
+        # reads in password policy, returns variables as a list. Written by KW
         # password_policy = open('password_policy.txt', 'r')
-        num_specials = 1
-        num_lowercase = 3
-        num_uppercase = 3
-        num_numbers = 4
-        min_length = 8
-        max_length = 16
-        allowed_specials = "~!\"£$%^&*()_-+="
+        policy = configparser.ConfigParser()
+        policy.read('password_policy.txt')
+        num_specials = policy.getint('Policy', 'num_specials')
+        num_lowercase = policy.getint('Policy', 'num_lowercase')
+        num_uppercase = policy.getint('Policy', 'num_uppercase')
+        num_numbers = policy.getint('Policy', 'num_numbers')
+        min_length = policy.getint('Policy', 'min_length')
+        max_length = policy.getint('Policy', 'max_length')
+        allowed_specials = policy.get('Policy', 'allowed_specials')
+
         return [int(num_specials), int(num_lowercase), int(num_uppercase), int(num_numbers), int(min_length),
                 int(max_length), allowed_specials]
 
